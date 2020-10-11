76b6f3e796eb0be59ec2399e3997f1113c987766
WeijieZheng708 <55973207+WeijieZheng708@users.noreply.github.com>
2020-10-12 01:17:01 +0800

    Update connect6MainWindow_1.h

--- connect6MainWindow_1.h
+++ connect6MainWindow_1.h
@@ -16,7 +16,7 @@ public:
 protected:
     // 绘制
     void paintEvent(QPaintEvent *event);
-    // 监听鼠标移动情况，方便落子
+    
  private:
     GameModel *game; // 游戏指针mouseMoveEvent
     GameType game_type; // 存储游戏类型
