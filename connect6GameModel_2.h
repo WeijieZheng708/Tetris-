32737f9bd67643f939d78fb59deebdec7db90ac9
WeijieZheng708 <55973207+WeijieZheng708@users.noreply.github.com>
2020-10-12 01:07:09 +0800

    Create connect6GameModel_2.h

---
+++ connect6GameModel_2.h
@@ -0,0 +1,39 @@
+#ifndef GAMEMODEL_H
+#define GAMEMODEL_H
+#include <vector>
+enum GameType
+{
+    PERSON,
+    Robet
+};
+
+// 游戏状态
+enum GameStatus
+{
+    PLAYING,
+    WIN,
+    DEAD
+};
+
+// 棋盘尺寸
+const int BoardSize = 20;
+
+class GameModel
+{
+public:
+    GameModel();
+
+public:
+    std::vector<std::vector<int>> ChessBoard; // 存储当前游戏棋盘和棋子的情况,空白为0，白子1，黑子-1
+    std::vector<std::vector<int>> Score; // 存储各个点位的评分情况，作为AI下棋依据
+    bool playerFlag; // 标示下棋方
+    GameType gameType; // 游戏模式
+    GameStatus gameStatus; // 游戏状态
+
+    void Start(GameType type); // 开始游戏
+    void calculateScore(); // 计算评分
+    void PVP(int row, int col); // 人执行下棋
+    void PVAI(int &clickRow, int &clickCol); // 机器执行下棋
+    };
+
+#endif // GAMEMODEL_H
